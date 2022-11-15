7b68b5c2c49ed9644ea435c513775ad27ea79672
Alejadnro Aparicio <torasemida@gmail.com>
2022-11-15 23:42:14 +0100

    first commit

--- js/xifres.js
+++ js/xifres.js
@@ -6,12 +6,14 @@ function xifres() {
     let nombre = prompt("Introdueix un nombre sencer");
     nombre = parseInt(nombre);
     let array = [];
+    const node = document.getElementById("demo");
 
     if (!Number.isInteger(nombre)) {
         document.getElementById("demo").innerHTML =
-            "El nombre no és sencer no es entero";
+            "El nombre no és sencer";
     } else {
-
+        document.getElementById("nombre").innerHTML +=
+        '<div class="col-md-3 numero_n" >' + nombre + '</div>';
         nombre = nombre.toString()
 
 
@@ -21,18 +23,28 @@ function xifres() {
 
             var caracter = nombre.charAt(i);
             caracter = parseInt(caracter);
+            document.getElementById("demo").innerHTML +=
+            '<div class="col-md-1 numero" >' + caracter + '</div>';
 
             array[i] = caracter;
 
-            array = array.sort(function(a, b){return a - b});
-            console.log(caracter);
+            array = array.sort(function(a, b){return a - b});         
+
+
 
         }   
         
+        for (var i = 0; i< array.length; i++) {
+
+            document.getElementById("ordenat").innerHTML +=
+            '<div class="col-md-1 numero" >' + array[i] + '</div>';
+
+
+        }
         console.log(array);
 
-        document.getElementById("demo").innerHTML =
-            "El nombre té " + quant;
+        
+            
 
     }
 
