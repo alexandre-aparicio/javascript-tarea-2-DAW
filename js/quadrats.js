b48ecceb5fa9a07f827226c757e6c1eee5467bf1
Alejadnro Aparicio <torasemida@gmail.com>
2022-11-16 20:57:13 +0100

    first commit

--- js/quadrats.js
+++ js/quadrats.js
@@ -1,45 +1,53 @@
-
-
-
 function quadrats() {
 
     let quadrat;
     let primer = prompt("Introdueix el primer nombre sencer");
     primer = parseInt(primer);
 
+    let num_01 = document.getElementById("num_01");
+    let num_02 = document.getElementById("num_02");
+    let error = document.getElementById("error");
+
     let segon = prompt("Introdueix un segon nombre sencer (Major que el primer)");
     segon = parseInt(segon);
-
-    console.log(Number.isInteger(primer)+" " + primer );
-    console.log(Number.isInteger(segon)+" " + segon );
     
     if (!Number.isInteger(primer) || !Number.isInteger(segon)) {
-        document.getElementById("demo").innerHTML =
-            "Hay alguno de tus números que no es entero";
+        
+        num_01.innerHTML = '?';
+        num_02.innerHTML = '?';
+        document.getElementById("error").innerHTML ="Qualcun del dos nombres no es sencer";
     }
 
     else if (primer >= segon) {
-        document.getElementById("demo").innerHTML =
-            "El primer no es MENOR que el segon";
+        num_01.innerHTML = '?';
+        num_02.innerHTML = '?';
+        error.innerHTML = 'El nombre primer no és menor que el segon';
     } else {
-
-        document.getElementById("demo").innerHTML =
-            "El primer és " + primer + " i el segon " + segon;
-
         
+        num_01.innerHTML = primer;
+        num_02.innerHTML = segon;
+
+            let resultats = false;
             while(primer<=segon)  
             {  
                 quadrat = (primer * primer);
                  if (quadrat > segon) {
                     break;
                  } else {
-                document.write("el nombre és " + primer + " i el seu quadrat és " + quadrat + "<br/>");  
 
+                    document.getElementById("quadrats").innerHTML += '<div class="col-md-1 p-4 numero numero_men text-center" >' + primer + '</div>';
                     primer++;
+                    resultats = true;
 
                  }
                 
             } 
+
+            if (!resultats) {
+                error.innerHTML = 'No hi ha resultats';
+            } else {
+                document.getElementById("encabezado").innerHTML = 'Resultats';
+            }
 
 
     }
